c7bb30d55b570aa9907469f9f510c9282e73db83
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-28 19:56:48 +0200

    added functionality to remove item from either list, all basic functionality working

--- js/logic.js
+++ js/logic.js
@@ -7,17 +7,23 @@ class List extends React.Component {
     constructor(props) {
         super(props);
         this.markAsDone = this.markAsDone.bind(this);
+        this.removeItem = this.removeItem.bind(this);
     }
+    //mark the selected item as done
     markAsDone(event) {
-        //console.log(event.target);
-        //console.log(event.target.id);
         this.props.handleMove(event.target);
+    }
+    //remove item from app
+    removeItem(event) {
+        if (event.target.classList.contains("remove")) {
+            this.props.removeItem(event.target);
+        } 
     }
     render() {
         var updatedList = this.props.list;
         var displayedList = [];
         for (let i = 0; i < updatedList.length; i++) {
-            displayedList.push(<li key={i} id={i}>{updatedList[i].task + " on " + updatedList[i].day + " " + updatedList[i].month + " " + updatedList[i].year}</li>)
+            displayedList.push(<li key={i} id={i} className={this.props.toRemove} onClick={this.removeItem}>{updatedList[i].task + " on " + updatedList[i].day + " " + updatedList[i].month + " " + updatedList[i].year}</li>)
         }
         return (
             <div>
@@ -33,16 +39,23 @@ class Done extends React.Component {
     constructor(props) {
         super(props);
         this.markAsToDo = this.markAsToDo.bind(this);
+        this.removeDoneItem = this.removeDoneItem.bind(this);
     }
+    //mark the selected item to move back to todo
     markAsToDo(event) {
-        //console.log(event.target);
         this.props.handleMoveBack(event.target);
+    }
+    //remove selected item from app
+    removeDoneItem(event) {
+        if (event.target.classList.contains("remove")) {
+            this.props.removeDoneItem(event.target);
+        } 
     }
     render() {
         var doneList = this.props.allDone;
         var displayedDone = [];
         for (let i = 0; i < doneList.length; i++) {
-            displayedDone.push(<li key={i} id={i}>{doneList[i].task + " on " + doneList[i].day + " " + doneList[i].month + " " + doneList[i].year}</li>)
+            displayedDone.push(<li key={i} id={i} className={this.props.toRemove} onClick={this.removeDoneItem}>{doneList[i].task + " on " + doneList[i].day + " " + doneList[i].month + " " + doneList[i].year}</li>)
         }
         return (
             <div>
@@ -59,7 +72,8 @@ class App extends React.Component {
         super(props);
         this.state = {
             listItems: [],
-            doneItems: []
+            doneItems: [],
+            toRemove: ""
         }
         this.getDay = this.getDay.bind(this);
         this.getMonth = this.getMonth.bind(this);
@@ -68,7 +82,11 @@ class App extends React.Component {
         this.handleAdd = this.handleAdd.bind(this);
         this.moveToDone = this.moveToDone.bind(this);
         this.moveBackToDo = this.moveBackToDo.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+        this.removeDoneItem = this.removeDoneItem.bind(this);
     }
+    //get the selected day month and year
     getDay(input) {
         this.day = input;
     }
@@ -81,6 +99,7 @@ class App extends React.Component {
     saveNewItem(input) {
         this.newTask = input;
     }
+    //add the user's input to the existing to do list
     handleAdd(event) {
         event.preventDefault();
         var newItem = {
@@ -97,60 +116,93 @@ class App extends React.Component {
             listItems: allItems
         })
     }
+    //move the selected item to the done list
     moveToDone(selectedItem) {
-        var sliceIt = selectedItem.textContent.indexOf("on");
-        var activity = selectedItem.textContent.slice(0, sliceIt);
-        var activityDate = selectedItem.textContent.split(/\s(?=\w)/);
-        var itemDone = {
-            task: activity,
-            day: activityDate[activityDate.length-3],
-            month: activityDate[activityDate.length-2],
-            year: activityDate[activityDate.length-1]
+        if (selectedItem.classList.contains("remove")) {
+            //console.log("dont move");
+            this.setState({
+                toRemove: ""
+            })
+        } else {
+            var sliceIt = selectedItem.textContent.indexOf("on");
+            var activity = selectedItem.textContent.slice(0, sliceIt);
+            var activityDate = selectedItem.textContent.split(/\s(?=\w)/);
+            var itemDone = {
+                task: activity,
+                day: activityDate[activityDate.length - 3],
+                month: activityDate[activityDate.length - 2],
+                year: activityDate[activityDate.length - 1]
+            }
+            var doneList = this.state.doneItems;
+            doneList.push(itemDone);
+            var done = this.state.listItems;
+            done.splice(selectedItem.id, 1);
+            this.setState({
+                listItems: done,
+                doneItems: doneList
+            })
         }
-        var doneList = this.state.doneItems;
-        doneList.push(itemDone);
-        var done = this.state.listItems;
-        done.splice(selectedItem.id, 1);
+    }
+    //move the selected item back to the todo list
+    moveBackToDo(selectedItem) {
+        if (selectedItem.classList.contains("remove")) {
+            //console.log("dont move");
+            this.setState({
+                toRemove: ""
+            })
+        } else {
+            var sliceIt = selectedItem.textContent.indexOf("on");
+            var activity = selectedItem.textContent.slice(0, sliceIt);
+            var activityDate = selectedItem.textContent.split(/\s(?=\w)/);
+            var itemToDo = {
+                task: activity,
+                day: activityDate[activityDate.length - 3],
+                month: activityDate[activityDate.length - 2],
+                year: activityDate[activityDate.length - 1]
+            }
+            //console.log(itemToDo);
+            var toDoList = this.state.listItems;
+            toDoList.push(itemToDo);
+            var toDo = this.state.doneItems;
+            toDo.splice(selectedItem.id, 1);
+            this.setState({
+                listItems: toDoList,
+                doneItems: toDo
+            })
+        }
+    }
+    //remove the selected todo item
+    handleRemove() {
+        //console.log("working");
         this.setState({
-            listItems: done,
-            doneItems: doneList
+            toRemove: "remove"
         })
     }
-    moveBackToDo(selectedItem) {
-        console.log(selectedItem);
-        var sliceIt = selectedItem.textContent.indexOf("on");
-        var activity = selectedItem.textContent.slice(0, sliceIt);
-        console.log(activity);
-        var activityDate = selectedItem.textContent.split(/\s(?=\w)/);
-        console.log(activityDate);
-        var itemToDo = {
-            task: activity,
-            day: activityDate[activityDate.length-3],
-            month: activityDate[activityDate.length-2],
-            year: activityDate[activityDate.length-1]
-        }
-        //console.log(itemToDo);
-        var toDoList = this.state.listItems;
-        toDoList.push(itemToDo);
-        var toDo = this.state.doneItems;
-        toDo.splice(selectedItem.id, 1);
+    removeItem(itemToRemove) {
+        var currentToDoList = this.state.listItems;
+        currentToDoList.splice(itemToRemove.id, 1);
+        //console.log(currentToDoList);
         this.setState({
-            listItems: toDoList,
-            doneItems: toDo
+            listItems: currentToDoList
+        })
+    }
+    //remove the selected done item
+    removeDoneItem(itemToRemove) {
+        var currentDoneList = this.state.doneItems;
+        currentDoneList.splice(itemToRemove.id, 1);
+        this.setState({
+            doneItems: currentDoneList
         })
     }
     render() {
-        //var days=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
         var displayDays = [];
         for (let i = 0; i < date.days.length; i++) {
             displayDays.push(<option key={date.days[i]}>{date.days[i]}</option>)
         }
-        //var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
         var displayMonths = [];
         for (let i = 0; i < date.months.length; i++) {
             displayMonths.push(<option key={date.months[i]}>{date.months[i]}</option>)
         }
-        //var years=["2019","2020","2021","2022","2023"];
         var displayYears = [];
         for (let i = 0; i < date.years.length; i++) {
             displayYears.push(<option key={date.years[i]}>{date.years[i]}</option>)
@@ -170,8 +222,9 @@ class App extends React.Component {
                     </select>
                 </div>
                 <input type="submit" value="Add" onClick={this.handleAdd}></input>
-                <List list={this.state.listItems} handleMove={this.moveToDone}></List>
-                <Done allDone={this.state.doneItems} handleMoveBack={this.moveBackToDo}></Done>
+                <List list={this.state.listItems} handleMove={this.moveToDone} toRemove={this.state.toRemove} removeItem={this.removeItem}></List>
+                <Done allDone={this.state.doneItems} handleMoveBack={this.moveBackToDo} toRemove={this.state.toRemove} removeDoneItem={this.removeDoneItem}></Done>
+                <input type="submit" value="Remove" onClick={this.handleRemove}></input>
             </div>
         )
     }
